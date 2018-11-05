632a18eaf5aa64d2f55cfeb8a479f42cb9b6a4a3
alawammo <alawammo@oregonstate.edu>
2018-11-05 10:22:17 -0800

    get request, error handling isn't working yet

--- tweetflask.py
+++ tweetflask.py
@@ -5,20 +5,49 @@ import json
 
 app = Flask(__name__)
 api = Api(app)
-data = json.load(open('db.json'))
+app.config['JSON_SORT_KEYS'] = False
+
+
+
 
 @app.route('/tweets')
 def get():
-    app.logger.info('here?????')
+    jsondata = json.load(open('db.json'))
     username = request.args.get('username')
-    #return username
-    for id in data['UserData']:
-        if username == id['Username'] or username == id['UserID']:
-            userID = id['UserID']
-            for tweets in data['TweetData']:
-                if userID == tweets['UserID']:
-                    return jsonify(tweets)
+    id = request.args.get('id')
 
+    try:
+        for user in jsondata['UserData']:
+            if username != None:
+                if username == user['Username']:
+                    return jsonify(get_tweet(jsondata, user))
+            if id != None:
+                if id == user['UserID']:
+                    return jsonify(get_tweet(jsondata, user))
+    except TypeError:
+        return "Invalid input!"
+
+def get_tweet(jsondata, user):
+    tweetdata = {
+        "data": [
+        ]
+    }
+    for tweets in jsondata['TweetData']:
+        if user['UserID'] == tweets['UserID']:
+            response_data = {}
+            response_data['id'] = user['UserID']
+            response_data['type'] = "string"
+            response_data['attributes'] = {
+                "username": user['Username'],
+                "userFullName": user['UserFullName'],
+                "timeAndDate": tweets['TimeAndDate'],
+                "title": tweets['Title'],
+                "mood": tweets['Mood'],
+                "tweet": tweets['TweetMsg']
+            }
+            tweetdata["data"].append(response_data)
+            #app.logger.info((tweetdata))
+    return tweetdata
 
 #api.add_resource(GetTweetsByUser, '/tweets?username=<string:username>')
 
