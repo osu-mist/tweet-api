867db6f99310996a77598ccd8ad6c34043989603
alawammo <alawammo@oregonstate.edu>
2018-11-06 13:17:54 -0800

    removed files

--- tweetflask.py
+++ tweetflask.py
@@ -1,28 +1,29 @@
 from flask import Flask, jsonify, Response, request
-from flask_restful import reqparse, abort, Api, Resource
+from flask_restful import Api, Resource
 import json
-
 app = Flask(__name__)
 api = Api(app)
 app.config['JSON_SORT_KEYS'] = False
+
 
 @app.route('/tweets')
 def get():
     jsondata = json.load(open('db.json'))
     username = request.args.get('username')
     id = request.args.get('id')
-    if username == None and id == None:
-        return jsonify(errors('400','No username or ID provided')), 400
+    if username is None and id is None:
+        return jsonify(errors('400', 'No username or ID provided')), 400
 
     for user in jsondata['UserData']:
-        if username != None:
+        if username is not None:
             if username == user['Username']:
                 return jsonify(get_tweet(jsondata, user))
-        return jsonify(errors('400','The username is invalid or don\'t exist')), 400
-        if id != None:
+        return jsonify(errors('400', 'The username is invalid')), 400
+        if id is not None:
             if id == user['UserID']:
                 return jsonify(get_tweet(jsondata, user))
-        return jsonify(errors('400','The ID is invalid or don\'t exist')), 400
+        return jsonify(errors('400', 'The id is invalid')), 400
+
 
 def get_tweet(jsondata, user):
     tweetdata = {
@@ -43,11 +44,13 @@ def get_tweet(jsondata, user):
                 "tweet": tweets['TweetMsg']
             }
             tweetdata["data"].append(response_data)
-            #app.logger.info((tweetdata))
     return tweetdata
 
+
 def errors(status, detail):
-    aboutlink = "https://developer.oregonstate.edu/documentation/error-reference#"
+    aboutlink = "https://developer.oregonstate.edu/documentation/error-reference"
+    if status is '400':
+        title = "Username is invalid"
     error = {
         "errors": [
         ]
@@ -55,19 +58,14 @@ def errors(status, detail):
     response_error = {}
     response_error['status'] = status
     response_error['links'] = {
-        "about": aboutlink + '1' + status
+        "about": f'{aboutlink}#1{status}'
     }
-    response_error['code'] = '1'+ status
-    response_error['title'] = 'Username is invalid'
+    response_error['code'] = f'1{status}'
+    response_error['title'] = title
     response_error['detail'] = detail
     error['errors'].append(response_error)
-    app.logger.info(error)
     return error
 
-@app.route('/tweets/<int:num>')
-def getbyid():
-    app.logger.info(("--------------"))
-    return "------------"
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
